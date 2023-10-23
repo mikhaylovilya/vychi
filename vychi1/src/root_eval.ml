e8def052422cfc17ba9956d61ed646f4c852ffd1
mikhaylovilya <ily2407@gmail.com>
2023-10-24 02:30:46 +0300

    vychi1: implement and test newton's method

--- vychi1/src/root_eval.ml
+++ vychi1/src/root_eval.ml
@@ -24,13 +24,21 @@ type answer =
 
 type conf =
   { f : float -> float
+  ; df : (float -> float) option
   ; interval : interval
   ; split_count : int
   ; epsilon : float
   }
 
+(* why the hell should i do that? *)
+let ( <. ) = Float.( < )
+let ( >. ) = Float.( > )
+let ( <=. ) = Float.( <= )
+let ( >=. ) = Float.( >= )
+
 module Eval = struct
   type f = float -> float
+  type df = float -> float
 
   let get_middle interval = (interval.right_b +. interval.left_b) /. 2.
 
@@ -38,9 +46,9 @@ module Eval = struct
     let h = (interval.right_b -. interval.left_b) /. Int.to_float split_count in
     let rec loop f interval h acc =
       match interval.left_b +. h with
-      | new_lb when Float.( <= ) new_lb interval.right_b ->
+      | new_lb when new_lb <=. interval.right_b ->
         let new_acc =
-          if Float.( <= ) (f interval.left_b *. f new_lb) 0.
+          if f interval.left_b *. f new_lb <=. 0.
           then { interval with right_b = new_lb } :: acc
           else acc
         in
@@ -51,22 +59,15 @@ module Eval = struct
   ;;
 
   let bisect_step f interval epsilon =
-    (* let halve_interval interval =
-      let middle = get_middle interval in
-      { interval with right_b = middle }, { interval with left_b = middle }
-    in *)
     let rec loop f interval epsilon iters =
-      (* let () =
-         if debug then Caml.Format.printf "a: %f, b: %f\n" interval.left_b interval.right_b
-         in *)
       let middle = get_middle interval in
       let halved_interval =
-        if Float.( <= ) (f interval.left_b *. f middle) 0.
+        if f interval.left_b *. f middle <. 0.
         then { interval with right_b = middle }
         else { interval with left_b = middle }
       in
       match halved_interval with
-      | { left_b; right_b } when Float.( < ) (right_b -. left_b) (2. *. epsilon) ->
+      | { left_b; right_b } when right_b -. left_b <. 2. *. epsilon ->
         let last_interv, approx_root, delta =
           Some { left_b; right_b }, (left_b +. right_b) /. 2., (right_b -. left_b) /. 2.
         in
@@ -88,8 +89,34 @@ module Eval = struct
     partition |> List.map ~f:(fun int -> bisect_step conf.f int conf.epsilon)
   ;;
 
-  let newton_step = ()
-  let newton_method = ()
+  let newton_step f df interval epsilon =
+    let df =
+      match df with
+      | Some x -> x
+      | None ->
+        let exception No_derivative of string in
+        raise (No_derivative "No derivative for function was provided")
+    in
+    let initial_root = get_middle interval in
+    let rec loop f df epsilon prev_root iters =
+      match prev_root -. (f prev_root /. df prev_root) with
+      | curr_root when Float.abs (curr_root -. prev_root) <. epsilon ->
+        let last_interv, approx_root, delta =
+          None, curr_root, Float.abs (curr_root -. prev_root) /. 2.
+        in
+        last_interv, approx_root, delta, iters
+      | root -> loop f df epsilon root (iters + 1)
+    in
+    let last_interv, approx_root, delta, iters = loop f df epsilon initial_root 0 in
+    { meth = Newton; initial_root; iters; last_interv; approx_root; delta }
+  ;;
+
+  let newton_method conf =
+    let partition = root_separation conf.f conf.interval conf.split_count in
+    partition
+    |> List.map ~f:(fun interval -> newton_step conf.f conf.df interval conf.epsilon)
+  ;;
+
   let mod_newton_step = ()
   let mod_newton_method = ()
   let secant_step = ()
@@ -145,7 +172,7 @@ end
 
 module Eq = struct
   let float_precision f1 f2 precision =
-    if Float.( < ) (Float.abs (f1 -. f2)) (1. **. (-1. *. Int.to_float precision))
+    if Float.abs (f1 -. f2) <. 1. **. (-1. *. Int.to_float precision)
     then true
     else Float.equal f1 f2
   ;;
@@ -156,14 +183,17 @@ module Eq = struct
   ;;
 
   let ans ans1 ans2 =
-    match ans1.last_interv, ans2.last_interv with
-    | None, _ | _, None -> true
-    | Some li1, Some li2 ->
-      float_precision ans1.initial_root ans2.initial_root 9
-      && Int.equal ans1.iters ans2.iters
-      && float_precision ans1.approx_root ans2.approx_root 9
-      && interval li1 li2
-      && float_precision ans1.delta ans2.delta 9
+    let last_interv_flag =
+      match ans1.last_interv, ans2.last_interv with
+      | None, None -> true
+      | Some li1, Some li2 -> interval li1 li2
+      | _ -> false
+    in
+    last_interv_flag
+    && float_precision ans1.initial_root ans2.initial_root 9
+    && Int.equal ans1.iters ans2.iters
+    && float_precision ans1.approx_root ans2.approx_root 9
+    && float_precision ans1.delta ans2.delta 9
   ;;
 
   let ans_list al1 al2 = List.equal ans al1 al2
@@ -171,6 +201,7 @@ end
 
 let conf : conf =
   { f = (fun x -> (x **. 2.) -. 5.)
+  ; df = Some (fun x -> 2. *. x)
   ; interval = { left_b = -5.; right_b = 5. }
   ; split_count = 50
   ; epsilon = 1.E-8
@@ -202,7 +233,7 @@ let%test "bisect_test1" =
   let res = Eval.bisect_step conf.f interval conf.epsilon in
   let expected_res : answer =
     { meth = Bisect
-    ; initial_root = Eval.get_middle interval
+    ; initial_root = -2.300000000
     ; iters = 23
     ; last_interv = Some { left_b = -2.236067986; right_b = -2.236067975 }
     ; approx_root = -2.236067981
@@ -216,7 +247,7 @@ let%test "bisect_test1" =
 
 let%test "bisect_method_test1" =
   (* let conf = { conf with epsilon = 1.E-3 } in *)
-  let () = Log.str "bisect_method1:" in
+  let () = Log.str "bisect_method_test1:" in
   let res = Eval.bisect_method conf in
   let expected_res : answer list =
     [ { meth = Bisect
@@ -232,6 +263,56 @@ let%test "bisect_method_test1" =
       ; last_interv = Some { left_b = 2.236067975; right_b = 2.236067986 }
       ; approx_root = 2.236067981
       ; delta = 0.000000006
+      }
+    ]
+  in
+  let () =
+    let () =
+      match res with
+      | [] -> Caml.Format.printf "No roots\n"
+      | lst -> lst |> Log.ans_list debug
+    in
+    expected_res |> Log.ans_list debug
+  in
+  Eq.ans_list res expected_res
+;;
+
+let%test "newton_test1" =
+  let () = Log.str "newton_test1:" in
+  let interval = { left_b = -2.400000; right_b = -2.200000 } in
+  let res = Eval.newton_step conf.f conf.df interval conf.epsilon in
+  let expected_res : answer =
+    { meth = Newton
+    ; initial_root = -2.300000000
+    ; iters = 3
+    ; last_interv = None
+    ; approx_root = -2.236067977
+    ; delta = 0.000000000
+    }
+  in
+  let () = Log.ans_list debug [ res ] in
+  let () = Log.ans_list debug [ expected_res ] in
+  Eq.ans res expected_res
+;;
+
+let%test "newton_method_test1" =
+  (* let conf = { conf with epsilon = 1.E-3 } in *)
+  let () = Log.str "newton_method_test1:" in
+  let res = Eval.newton_method conf in
+  let expected_res : answer list =
+    [ { meth = Bisect
+      ; initial_root = -2.300000000
+      ; iters = 3
+      ; last_interv = None
+      ; approx_root = -2.236067977
+      ; delta = 0.000000000
+      }
+    ; { meth = Bisect
+      ; initial_root = 2.300000000
+      ; iters = 3
+      ; last_interv = None
+      ; approx_root = 2.236067977
+      ; delta = 0.000000000
       }
     ]
   in
